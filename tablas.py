254d7c15431467f9a59445a65a477751a3151938
Jose Luis Acuña <jose8club@gmail.com>
2013-06-24 23:36:02 -0400

    Hecha en pyside

---
+++ tablas.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+from PySide import QtGui, QtCore
+
+
+
+class Main(QtGui.QWidget):
+	def __init__(self):
+		"""Documento de la clase main"""
+		super(Main, self).__init__()
+		self.resize(900, 600)
+		self.layout = QtGui.QVBoxLayout(self)
+		self.layouts()
+		self.tables()
+		#self.datos()
+		#self.signals()
+		self.show()
+
+	def layouts(self):
+		self.l = QtGui.QWidget(self)
+		self.hl = QtGui.QHBoxLayout()
+		self.hl.setAlignment(QtCore.Qt.AlignRight)
+		self.l.setLayout(self.hl)
+		##botones y combobox y filtro
+		self.label = QtGui.QLabel(u"Ciudad")
+		self.filtro = QtGui.QLineEdit()
+		self.combo = QtGui.QComboBox(self)
+		#self.combo.activated[int].connect(self.onActivated)
+		self.b1 = QtGui.QPushButton(u"&Buscar")
+		self.b3 = QtGui.QPushButton(u"&Eliminar local")
+		self.hl.addWidget(self.label)
+		self.hl.addWidget(self.filtro)
+		self.hl.addWidget(self.combo)
+		self.hl.addWidget(self.b1)
+		
+		self.hl.addWidget(self.b3)
+		##agreagar este layout al vertical
+		self.layout.addWidget(self.l)
+
+	def tables(self):
+		self.table = QtGui.QTableWidget(self)
+		
+		self.table.setFixedWidth(890)
+		self.table.setFixedHeight(480)
+		self.table.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+		self.table.setAlternatingRowColors(True)
+		self.table.setSortingEnabled(True)
+
+		self.layout.addWidget(self.table)
+
+	def signals(self):
+		self.b1.clicked.connect(self.create)
+		self.b3.clicked.connect(self.borrar)
+
+	def borrar(self):
+		modelo = self.table.model()
+		index = self.table.currentIndex()
+		if index.row() == -1: #No se ha seleccionado una fila
+			self.errorMessageDialog = QtGui.QErrorMessage(self)
+			self.errorMessageDialog.showMessage("Debe seleccionar una fila")
+			return False
+
+
+
+def run():
+	app = QtGui.QApplication(sys.argv)
+	main = Main()
+	sys.exit(app.exec_())
+
+if __name__ == '__main__':
+	run()
+
+
+
